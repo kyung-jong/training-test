89c16d91ef4b75c6b3f2d4f8bc87b641c688c8d4
kyoung-jong <qusrudwhd@gmail.com>
2024-01-18 23:46:17 +0900

    first commit

---
+++ 1_basics/5_data_types.js
@@ -0,0 +1,26 @@
+
+const age = 32;
+const tempature = -10;
+const pi = 3.14;
+
+console.log(typeof age);
+console.log(typeof tempature);
+console.log(typeof pi);
+
+
+const infinity = Infinity;
+console.log(infinity);
+
+/**
+ * Template Literal
+ */
+
+const test1 = `동해물과 "백두산이" / 마르고 닳도록 ~! ''`;
+console.log(test1);
+
+let cc = 'cc';
+console.log(cc);
+cc = `cc"c`;
+console.log(cc);
+
+
