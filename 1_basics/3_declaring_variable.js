4c61ddddff6d99fc97252893c84b282e79320a01
kyoung-jong <qusrudwhd@gmail.com>
2024-01-21 23:14:20 +0900

    다시올림

---
+++ 1_basics/3_declaring_variable.js
@@ -0,0 +1,17 @@
+/**
+ * Variable 선언하기
+ * 
+ * 1) var - 더이상 쓰지 않는다.
+ * 
+ */
+
+let test = 'test';
+const aaa = 'aaa';
+
+console.log(test);
+console.log(aaa);
+
+test = 'test2';
+// aaa = 'aaa';
+
+
