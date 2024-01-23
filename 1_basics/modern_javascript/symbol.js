1f8a6d585640eadd1e9f0396ed73ff95646c602c
kdl_bkj <qusrudwhd@gmail.com>
2024-01-23 19:11:39 +0900

    aa

--- 1_basics/modern_javascript/symbol.js
+++ 1_basics/modern_javascript/symbol.js
@@ -15,3 +15,19 @@ let user = {
 let id = Symbol("id");
 user[id] = 1;
 console.log(user[id]);
+
+
+for (let key in user) {
+    console.log(key);
+};
+
+/** Object.assign은 키가 심볼인 프로퍼티를 배제하지 않고 객체 내 모든 프로퍼티를 복사한다. */
+let clone = Object.assign({}, user);
+console.log(clone[id]);
+
+
+console.log('----------------------------');
+
+let id2 = Symbol.for("id");
+let id2Again = Symbol.for("id");
+console.log(id2 === id2Again);
