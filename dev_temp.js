793b399a200dabb2dfa85309c428579ff4e392cb
kdl_bkj <qusrudwhd@gmail.com>
2024-01-24 10:31:40 +0900

    conflict dev_temp.js

--- dev_temp.js
+++ dev_temp.js
@@ -1,9 +1,10 @@
 
-let searchText = '제출완료';
-let replaceText = '신청완료';
+const searchText = '제출완료';
+const replaceText = '신청완료';
 
 let currentText = '\n\t\t\t\t\t\t\t\t\t전체\n\t\t\t\t\t\t\t\t\t제출완료보완요청승인반려\n\t\t\t\t\t\t\t\t';//$('#search_atrz_sttus_code').text();
 console.log(currentText);
 var nextText = currentText.replace(searchText, replaceText);
 
 console.log(nextText);
+
